c4d40796ff26d999536d2f03a1cc9db43604ae5d
Dharanitharan-I <65530644+Dharanitharan-I@users.noreply.github.com>
2021-05-10 02:50:27 +0530

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,24 @@
+
+#include <Servo.h>
+String reads;
+Servo myservo;
+
+void setup() {
+  myservo.attach(9); 
+  Serial.begin(9600);// attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  while(Serial.available()==0);
+  reads=Serial.readStringUntil('\n');
+  if (reads=="adrfjhlnm")
+  {
+   myservo.write(0);
+   // waits for the servo to get there
+  }
+  if (reads=="wrong")
+  {
+     myservo.write(79);
+  }
+}
+
